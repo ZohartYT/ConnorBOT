88ea562dfb3d0357ad697d43013463d084bd65a5
ZohartYT <40930644+ZohartYT@users.noreply.github.com>
2018-07-08 17:10:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,28 +7,28 @@ robot.login("NDY1NDEwOTA0ODM4ODk3NjY1.DiNHRg.I6Fu4ilwzc3oKdXVoNdwZLCQ5eQ");
 
 robot.on('message', (message)=> {
     if(message.content == prefix + "help") {
-        message.channel.send("```****"
-        + "**Мои команды : **\n"
-        + "**.cake**\n"
-        + "**.info**\n"
-        + "**.help**\n"
-        + "**Вот список моих команд!**```\n");
+        message.channel.send("```"
+        + "Мои команды : \n"
+        + ".cake\n"
+        + ".info\n"
+        + ".help\n"
+        + "Вот список моих команд!```\n");
     }
     if(message.content == prefix + "cake") {
-        message.channel.send("```****"
-        + "**Да, я дюблю тортики!**```\n");
+        message.channel.send("```"
+        + "Да, я дюблю тортики!```\n");
     }
     if(message.content == prefix + "info") {
-        message.channel.send("```****"
-        + "**Мой создатель : Zohart**\n"
-        + "**Моя версия : 1.0.0 OBT**\n"
-        + "**Я могу помогать!**```\n");
+        message.channel.send("```"
+        + "Мой создатель : Zohart\n"
+        + "Моя версия : 1.0.0 OBT\n"
+        + "Я могу помогать!```\n");
     }
     if(message.content == prefix + "info") {
-        message.channel.send("```****"
-        + "**Мой создатель : Zohart\n"
-        + "**Моя версия : 1.0.0 OBT**\n"
-        + "**Я могу помогать!**```\n");
+        message.channel.send("```"
+        + "Мой создатель : Zohart\n"
+        + "Моя версия : 1.0.0 OBT**\n"
+        + "Я могу помогать!```\n");
     }
     
 });
