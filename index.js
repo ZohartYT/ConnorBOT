bc2bbd44205f530384f326bd6d1299fbb648b0d2
ZohartYT <40930644+ZohartYT@users.noreply.github.com>
2018-07-08 17:17:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -30,3 +30,12 @@ robot.on('message', (message)=> {
     }
     
 });
+
+robot.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
