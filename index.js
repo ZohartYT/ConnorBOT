6017eb3ffe67c54d68f803b33f3c1d50e13ef294
ZohartYT <40930644+ZohartYT@users.noreply.github.com>
2019-02-05 15:54:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const robot = new Discord.Client();
+const channel = robot.channels.find('чат', channelName)
 
 var prefix = "cb!";
 if(process.env.BOT_TOKEN == '') {
@@ -47,12 +48,21 @@ robot.on('message', (message)=> {
         + "И какой человек меня создал? Ответ : Zohart\n"
         + "Я был написан быстро, но чётко по плану.\n"
         + "В игре, я расследую преступления, но в Discord, я помогаю людям :D\n"
-        + "Хочешь узнать мои команды? Напиши '.help'```");
+        + "Хочешь узнать мои команды? Напиши 'cb!help'```");
     }
     if (message.content === prefix + "detroitImg") {
         message.reply("https://2ch.hk/v/src/3031203/15274500811210.png");
     }
     if (message.content === prefix + "invite") {
         message.reply("распростроняй эту ссылку (https://discord.gg/H7yywKJ) и нас станет больше!");   
+    }
+    if (message.content === prefix + "postNews") {
+        const msgfN = replace('cb!postNews ', '', message.content)
+        channel.send('- @everyone @here -\n'
+        + '```Новости!```\n'
+        + msgfN + '\n'
+        + '\n'
+        + 'Спасибо за внимание\n'
+        + 'Created by Zohart');
     }
 });
